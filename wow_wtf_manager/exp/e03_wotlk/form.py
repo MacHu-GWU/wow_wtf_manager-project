1dac5f07c749165ca6e29873e1858bc11073fe73
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-08-25 10:07:45 -0400

    add all_characters

--- wow_wtf_manager/exp/e03_wotlk/form.py
+++ wow_wtf_manager/exp/e03_wotlk/form.py
@@ -40,19 +40,6 @@ from .wtf import (
 )
 
 
-# @attr.s
-# class Asso(AttrsClass):
-#     """
-#     WTF 配置 和 游戏角色 之间的关联.
-#
-#     这里的 config 对象无需给 dir_wow, account, server, character 这些属性赋值.
-#     config 只要定义了 input_path, 知道从哪里读配置数据即可. 而 group 中的 Character
-#     对象会补充这些信息.
-#     """
-#     config: BaseConfig = attr.ib()
-#     group: CharacterGroup = attr.ib()
-
-
 @attr.s
 class WtfForm(AttrsClass):
     """
@@ -101,6 +88,8 @@ class WtfForm(AttrsClass):
         T.Tuple[CharacterSavedVariablesConfig, OrderedSet[Character]]
     ] = attr.ib(factory=list)
 
+    all_characters: OrderedSet[Character] = attr.ib(factory=OrderedSet)
+
     def __attrs_post_init__(self):
         self.game_client_config.dir_wow = self.dir_wow
         for key, value in attr.asdict(self, recurse=False).items():
@@ -141,7 +130,7 @@ class WtfForm(AttrsClass):
             config_content_mapper: T.Dict[str, str] = dict()
             for p in config.lua_file_list:
                 tpl = Template(p.read_text())
-                content = tpl.render(all_characters=self.all_characters)
+                content = tpl.render(all_characters=self._all_characters)
                 config_content_mapper[p.basename] = content
 
             for account in account_orderedset:
@@ -206,7 +195,7 @@ class WtfForm(AttrsClass):
             config_content_mapper: T.Dict[str, str] = dict()
             for p in config.lua_file_list:
                 tpl = Template(p.read_text())
-                content = tpl.render(all_characters=self.all_characters)
+                content = tpl.render(all_characters=self._all_characters)
                 config_content_mapper[p.basename] = content
 
             for character in character_orderedset:
@@ -233,74 +222,15 @@ class WtfForm(AttrsClass):
         self.apply_character_macro_config()
         self.apply_character_saved_variables_config()
 
-    #     groups: T.Dict[str, dict] = {
-    #         GameClientConfig.__name__: {
-    #             "method": "apply_game_client_config",
-    #             "associations": [],
-    #         },
-    #         AccountUserInterfaceConfig.__name__: {
-    #             "method": "apply_account_user_interface_config",
-    #             "associations": [],
-    #         },
-    #         AccountMacroConfig.__name__: {
-    #             "method": "apply_account_macro_config",
-    #             "associations": [],
-    #         },
-    #         AccountSavedVariablesConfig.__name__: {
-    #             "method": "apply_account_saved_variables_config",
-    #             "associations": [],
-    #         },
-    #         AccountKeybindingConfig.__name__: {
-    #             "method": "apply_account_keybinding_config",
-    #             "associations": [],
-    #         },
-    #         AccountCacheConfig.__name__: {
-    #             "method": "apply_account_cache_config",
-    #             "associations": [],
-    #         },
-    #         CharacterUserInterfaceConfig.__name__: {
-    #             "method": "apply_character_user_interface_config",
-    #             "associations": [],
-    #         },
-    #         CharacterChatConfig.__name__: {
-    #             "method": "apply_character_chat_config",
-    #             "associations": [],
-    #         },
-    #         CharacterKeybindingConfig.__name__: {
-    #             "method": "apply_character_keybinding_config",
-    #             "associations": [],
-    #         },
-    #         CharacterLayoutConfig.__name__: {
-    #             "method": "apply_character_layout_config",
-    #             "associations": [],
-    #         },
-    #         CharacterAddonConfig.__name__: {
-    #             "method": "apply_character_addon_config",
-    #             "associations": [],
-    #         },
-    #         CharacterMacroConfig.__name__: {
-    #             "method": "apply_character_macro_config",
-    #             "associations": [],
-    #         },
-    #         CharacterSavedVariablesConfig.__name__: {
-    #             "method": "apply_character_saved_variables_config",
-    #             "associations": [],
-    #         },
-    #     }
-    #     for asso in self.associations:
-    #         groups[asso.config.__class__.__name__]["associations"].append(asso)
-    #
-    #     for key, value in groups.items():
-    #         getattr(self, value["method"])(value["associations"])
-
     @functools.cached_property
-    def all_characters(self) -> OrderedSet[Character]:
+    def _all_characters(self) -> OrderedSet[Character]:
         """
         把所有在 WtfForm 中所有涉及到的 Character 收集起来, 去重后形成一个列表后返回.
 
         这是因为在 SavedVariables 里面插件的配置是用 Profile 来管理的. 然后你用不同的角色
         登录后就根据 Profile Name 选择已经定义好的或是默认值. 这就需要虽然我们只对一个
         """
+        char_set_list = [self.all_characters, ]
         char_set_list = list()
         for key, value in attr.asdict(self, recurse=False).items():
             if key.startswith("character_"):
