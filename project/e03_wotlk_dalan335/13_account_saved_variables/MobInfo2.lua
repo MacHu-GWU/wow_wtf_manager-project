2b62f754d1d129c6b31bae7ca1bb17583c8bbad4
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2022-09-02 12:13:04 -0400

    Create MobInfo2.lua

---
+++ project/e03_wotlk_dalan335/13_account_saved_variables/MobInfo2.lua
@@ -0,0 +1,75 @@
+
+MobInfoConfig = {
+	["HealthPosX"] = -7,
+	["UseGameTT"] = 0,
+	["SaveBasicInfo"] = 1,
+	["ShowQuality"] = 1,
+	["ShowIGrey"] = 0,
+	["ShowLoots"] = 1,
+	["ShowIBlue"] = 1,
+	["ShowKills"] = 0,
+	["ShowIGreen"] = 1,
+	["ShowDps"] = 1,
+	["ShowHealth"] = 1,
+	["ShowLocation"] = 1,
+	["HealthPosY"] = 11,
+	["ShowResists"] = 1,
+	["ManaPosX"] = -7,
+	["ShowCloth"] = 1,
+	["ShowXp"] = 1,
+	["CombinedMode"] = 0,
+	["TargetMana"] = 1,
+	["SaveItems"] = 1,
+	["ShowNo2lev"] = 1,
+	["SmallFont"] = 1,
+	["ShowMana"] = 0,
+	["SavePlayerHp"] = 0,
+	["ShowItemPrice"] = 0,
+	["TargetFontSize"] = 10,
+	["OtherTooltip"] = 1,
+	["ShowCoin"] = 0,
+	["ItemTooltip"] = 1,
+	["ShowClothSkin"] = 1,
+	["ShowLowHpAction"] = 1,
+	["ManaPercent"] = 1,
+	["SaveCharData"] = 1,
+	["ImportOnlyNew"] = 1,
+	["MMButtonPos"] = 257,
+	["ShowIWhite"] = 1,
+	["ShowIV"] = 0,
+	["SaveResist"] = 1,
+	["ShowImmuns"] = 1,
+	["MouseTooltip"] = 1,
+	["ShowItemInfo"] = 1,
+	["TargetHealth"] = 1,
+	["ShowEmpty"] = 0,
+	["ShowMMButton"] = 1,
+	["TargetFont"] = 2,
+	["KeypressMode"] = 0,
+	["HealthPercent"] = 1,
+	["ManaPosY"] = 11,
+	["ShowIPurple"] = 1,
+	["HideAnchor"] = 0,
+	["ShowDamage"] = 1,
+	["ShowTargetInfo"] = 0,
+	["CompactMode"] = 1,
+	["ShowTotal"] = 1,
+	["ItemsQuality"] = 2,
+	["ShowItems"] = 1,
+	["ItemFilter"] = "",
+	["TooltipMode"] = 4,
+	["ShowMobInfo"] = 1,
+	["DisableHealth"] = 0,
+}
+MobInfoDB = {
+}
+MobHealthDB = {
+}
+MobHealthPlayerDB = {
+}
+MI2_ItemNameTable = {
+}
+MI2_CharTable = {
+}
+MI2_ZoneTable = {
+}
