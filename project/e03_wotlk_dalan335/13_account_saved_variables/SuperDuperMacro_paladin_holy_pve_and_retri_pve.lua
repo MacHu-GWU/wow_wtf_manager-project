0c0a0ece241fd44e39de98c7f7764bab485b71fa
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2022-09-02 14:58:29 -0400

    c

---
+++ project/e03_wotlk_dalan335/13_account_saved_variables/SuperDuperMacro_paladin_holy_pve_and_retri_pve.lua
@@ -0,0 +1,23 @@
+
+sdm_version = "1.8.3"
+sdm_listFilters = {
+	["true"] = true,
+	["s"] = true,
+	["b"] = true,
+	["false"] = true,
+	["global"] = true,
+	["f"] = true,
+}
+sdm_iconSize = 36
+sdm_mainContents = {
+	0, -- [1]
+}
+sdm_macros = {
+	[0] = {
+		["type"] = "b",
+		["name"] = "MB-Heal-Rotate",
+		["ID"] = 0,
+		["text"] = "#showtooltip\n#/targetparty\n#/targetraid\n#/target focustargettarget\n/castsequence reset=30 圣光闪现,圣光闪现,圣光闪现,圣光闪现,圣光术,圣光闪现,圣光闪现,圣光闪现,圣光闪现,圣光术,圣光闪现,圣光闪现,圣光闪现,圣光闪现,圣光术,神圣恳求,圣光闪现,圣光闪现,圣光闪现,圣光闪现,圣光术,圣光闪现,圣光闪现,圣光闪现,圣光闪现,圣光术,圣光闪现,圣光闪现,圣光闪现,圣光闪现,圣光术",
+		["icon"] = 1,
+	},
+}
