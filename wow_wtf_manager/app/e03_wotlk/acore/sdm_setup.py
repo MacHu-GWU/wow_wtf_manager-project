e4103d68ba34fbef64fe8317bef2783d4a523ab3
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-11-06 23:26:48 -0500

    Update sdm_setup.py

--- wow_wtf_manager/app/e03_wotlk/acore/sdm_setup.py
+++ wow_wtf_manager/app/e03_wotlk/acore/sdm_setup.py
@@ -26,6 +26,7 @@ for account in [
     account_sdm_setup = AccountSDMSetup(
         account=account,
         macros=[
+            Macros.sdm_07_druid____0_common____16101_consumable.macro,
             Macros.sdm_07_druid____1_balance_resto____16401_buff_self_zhTW.macro,
             Macros.sdm_07_druid____1_balance_resto____16402_buff_raid_zhTW.macro,
             Macros.sdm_07_druid____1_balance_resto____16403_multibox_main_rotate_zhTW.macro,
