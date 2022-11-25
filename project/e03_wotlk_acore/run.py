4c6eab2fc5276395c1db76f9317516710ebda7a9
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2022-11-25 14:05:49 -0500

    c

--- project/e03_wotlk_acore/run.py
+++ project/e03_wotlk_acore/run.py
@@ -6,9 +6,9 @@ from wow_wtf_manager.app.e03_wotlk.acore.form import acore_wtf_form
 
 # acore_wtf_form.apply_game_client_config(plan=False)
 
-acore_wtf_form.apply_account_user_interface_config(plan=False)
+# acore_wtf_form.apply_account_user_interface_config(plan=False)
 # acore_wtf_form.apply_account_macro_config(plan=False)
-# acore_wtf_form.apply_account_saved_variables_config(plan=False)
+acore_wtf_form.apply_account_saved_variables_config(plan=True)
 
 # acore_wtf_form.apply_character_user_interface_config(plan=False)
 # acore_wtf_form.apply_character_chat_config(plan=False)
