e18ca827ff33a6db90a133946352dd525adcc7c5
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-10 21:50:22 -0400

    c

--- wow_wtf_manager/exp/e03_wotlk/api.py
+++ wow_wtf_manager/exp/e03_wotlk/api.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from .paths import AppData
+from .config import (
+    GameClientConfig,
+    AccountUserInterfaceConfig,
+    AccountSavedVariablesConfig,
+    CharacterUserInterfaceConfig,
+    CharacterKeybindingConfig,
+    CharacterAddonConfig,
+    CharacterLayoutConfig,
+    CharacterSavedVariablesConfig,
+)
--- wow_wtf_manager/exp/e03_wotlk/config.py
+++ wow_wtf_manager/exp/e03_wotlk/config.py
@@ -1,37 +1,76 @@
 # -*- coding: utf-8 -*-
 
+import typing as T
 import attr
 from ordered_set import OrderedSet
 
+from ...logger import logger
+from ...models.api import Client, Account, Character
+from ...scope.api import (
+    ClientScope,
+    AccountKeyBindingScope,
+    AccountUserInterfaceScope,
+    AccountAddonSavedVariablesScope,
+    CharacterKeyBindingScope,
+    CharacterChatScope,
+    CharacterUserInterfaceScope,
+    CharacterLayoutScope,
+    CharacterAddonSavedVariablesScope,
+)
 from ...config.api import FileConfig
-from ...models.api import Account, Character
 
 
 @attr.define
 class GameClientConfig(FileConfig):
-    pass
+    def apply(
+        self,
+        client: Client,
+        dry_run: bool = True,
+    ) -> bool:
+        content = self.build()
+        scope = ClientScope(client=client)
+        if dry_run is False:
+            scope.path_output.write_text(content)
+        return not dry_run
 
 
 @attr.define
 class AccountUserInterfaceConfig(FileConfig):
-    pass
+    def build(self, account: Account) -> str:
+        return self.template.render(account=account)
+
+    def apply(
+        self,
+        client: Client,
+        account: Account,
+        dry_run: bool = True,
+    ) -> bool:
+        content = self.build(account=account)
+        scope = AccountUserInterfaceScope(client=client, account=account)
+        if dry_run is False:
+            scope.path_output.write_text(content)
+        return not dry_run
 
 
 @attr.s
 class AccountSavedVariablesConfig(FileConfig):
-    def build(
+    def build(self, account: Account) -> str:
+        return self.template.render(account=account)
+
+    def apply(
         self,
+        client: Client,
         account: Account,
-        account_group: OrderedSet[Account],
-        character: Character,
-        character_group: OrderedSet[Character],
-    ) -> str:
-        return self.template.render(
-            account=account,
-            account_group=account_group,
-            character=character,
-            character_group=character_group,
+        addon: str,
+        dry_run: bool = True,
+    ) -> bool:
+        content = self.build(account=account)
+        scope = AccountAddonSavedVariablesScope(
+            client=client, account=account, addon=addon
         )
+        if dry_run is False:
+            scope.path_output.write_text(content)
+        return not dry_run
 
 
 @attr.define
@@ -58,10 +97,11 @@ class CharacterLayoutConfig(FileConfig):
 class CharacterSavedVariablesConfig(FileConfig):
     def build(
         self,
-        account: Account,
-        account_group: OrderedSet[Account],
-        character: Character,
-        character_group: OrderedSet[Character],
+        client: T.Optional[Client] = None,
+        account: T.Optional[Account] = None,
+        account_group: T.Optional[OrderedSet[Account]] = None,
+        character: T.Optional[Character] = None,
+        character_group: T.Optional[OrderedSet[Character]] = None,
     ) -> str:
         return self.template.render(
             account=account,
