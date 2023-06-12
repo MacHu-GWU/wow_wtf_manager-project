4a6aa1c1952acf17e75f62d40086f89d1137b06a
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-12 14:42:29 -0400

    add int test

---
+++ tests_int/test_app_e03_wotlk_acore_horde.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from wow_wtf_manager.logger import logger
+from wow_wtf_manager.app.e03_wotlk_acore_horde.api import setup, sdm_setup
+
+# dry_run = True # for testing
+dry_run = False  # *** THIS WILL APPLY CHANGES TO YOUR WTF FILEs ***
+
+
+def _test():
+    setup.client.dir_wtf.remove_if_exists()
+
+    setup.show_game_client()
+    setup.apply_game_client(dry_run=dry_run)
+    setup.apply_account_user_interface(dry_run=dry_run)
+    setup.apply_account_saved_variables(dry_run=dry_run)
+    setup.apply_character_user_interface(dry_run=dry_run)
+    setup.apply_character_chat(dry_run=dry_run)
+    setup.apply_character_keybinding(dry_run=dry_run)
+    setup.apply_character_layout(dry_run=dry_run)
+    setup.apply_character_addons(dry_run=dry_run)
+    setup.apply_character_saved_variables(dry_run=dry_run)
+
+    sdm_setup.apply(dry_run=dry_run)
+
+
+def test():
+    with logger.disabled(
+        disable=True,
+        # disable=False,
+    ):
+        print("")
+        _test()
+
+
+if __name__ == "__main__":
+    from wow_wtf_manager.tests import run_cov_test
+
+    run_cov_test(
+        __file__, "wow_wtf_manager.app.e03_wotlk_acore_horde", is_folder=True, preview=False
+    )
